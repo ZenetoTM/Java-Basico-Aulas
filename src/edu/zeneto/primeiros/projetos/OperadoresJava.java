e58e3318c99cf86b0f0eb901b82277336f60bfda
zeneto <jg515564721@gmail.com>
2024-05-11 14:26:35 -0300

    domingo1.2

---
+++ src/edu/zeneto/primeiros/projetos/OperadoresJava.java
@@ -0,0 +1,64 @@
+package edu.zeneto.primeiros.projetos;
+
+import java.sql.SQLOutput;
+
+public class OperadoresJava {
+    public static void main(String[] args) {
+
+        /*String nomeCompleto = "Linguagem " + "Java";
+
+         *System.out.println(nomeCompleto);
+
+         *String concatenacao = "?";
+          *concatenacao = 1+1+1+"1";
+
+         *System.out.println(concatenacao);
+
+          *concatenacao = 1+1+"1"+"1";
+
+         *System.out.println(concatenacao);
+
+          *concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);*/
+
+        /*int numero = 5;
+         numero = - numero;
+          System.out.println(numero);
+
+         numero = numero * -1;
+          System.out.println(numero); */
+
+       /* int numero = 5;
+
+        int i = numero++;
+        System.out.println(numero); */
+
+       /* boolean variavel = true;
+
+          System.out.println(!variavel);
+          System.out.println(variavel);
+
+        variavel = !variavel;
+
+           System.out.println(variavel); */
+
+        /*int a, b;
+
+        a = 5;
+        b = 6;
+        String resultado = a==b ?"Verdadeiro" : "falso";
+
+        /*if(a==b)
+            resultado = "Verdadeiro";
+
+        else
+            resultado = "Falso";*/
+
+        System.out.println(resultado);*/
+
+
+
+
+    }
+}
