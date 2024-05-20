fdbf5f62026a917e6900dd7ef47eb47bd3e8d432
zeneto <jg515564721@gmail.com>
2024-05-20 06:49:19 -0300

    avoura

--- src/edu/zeneto/primeiros/projetos/OperadoresJava.java
+++ src/edu/zeneto/primeiros/projetos/OperadoresJava.java
@@ -55,10 +55,84 @@ public class OperadoresJava {
         else
             resultado = "Falso";*/
 
-        System.out.println(resultado);*/
+        /* System.out.println(resultado); */
 
 
+      /* int numero1 = 1;
+        int numero2 = 2;
+
+        if (numero1 > numero2)
+            System.out.println("Numero 1 maior que Numero 2");
+
+        if (numero1 < numero2)
+            System.out.println("Numero 2 maior que Numero 1");
+
+        if (numero1 <= numero2)
+        System.out.println("Numero 2 maior ou igual que Numero 1");
+
+        if (numero1 >= numero2)
+        System.out.println("Numero 1 maior ou igual que Numero 2");
+
+        if (numero1 != numero2)
+            System.out.println("Numero 1 é diferente de Numero 2"); */
 
 
+        /*char numero1 = 1;
+        char numero2 = 2;
+
+        boolean simNão = numero1 == numero2;
+
+        System.out.println("Numero 1 é igual ou maior que o numero 2 ? " + simNão);
+
+        simNão = numero1 != numero2;
+
+        System.out.println("Numero 1 é diferente que o numero 2 ? " + simNão); */
+
+
+        /*String nomeUm = "José";
+        String nomeDois = new String("José");
+
+        if (nomeUm.equals(nomeDois)) {
+            System.out.println("Isso está certo");
+
+        }
+
+            else {
+            System.out.println("errado"); }*/
+
+        /*String nomeUm = "Jos";
+        String nomeDois = new String("José");
+
+        if (nomeUm.equals(nomeDois)) {
+            System.out.println("Isso está certo");
+
+        }
+
+        else {
+            System.out.println("errado"); }*/
+
+
+       /* boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        if (condicao1 && condicao2) {
+            System.out.println("às duas condições são verdadeiras");
+        } else {
+            System.out.println("tá errado corno");
+        }
+
+        condicao2 = true;
+
+        if (condicao1 && condicao2){
+            System.out.println("ãs duas condições são verdadeiras");
+
+
+        }
+        else {
+            System.out.println("tá errrado corno"); }
+        if (condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira"); */
+        }
     }
 }
