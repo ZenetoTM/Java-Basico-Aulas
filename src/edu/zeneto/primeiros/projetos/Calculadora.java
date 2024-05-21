812542e95cd95ba3e52b47b8f8accad24c5b571f
zeneto <jg515564721@gmail.com>
2024-05-21 08:44:23 -0300

    abobad21/051

---
+++ src/edu/zeneto/primeiros/projetos/Calculadora.java
@@ -0,0 +1,27 @@
+package edu.zeneto.primeiros.projetos;
+
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza  operações matemática entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classes
+ * para desfrutar dos recursos oferecidos pelo autor
+ *
+ * @author José
+ * @version 1.0
+ * @since 21/05/2024
+ */
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     *
+     * @param numeroUM   este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar(int numeroUM, int numeroDois) {
+        return numeroUM + numeroDois;
+    }
+
+
+}
