a487734115c36178d1f0062ee5bee44c909255b6
zeneto <jg515564721@gmail.com>
2024-05-26 08:27:23 -0300

    d26/05

---
+++ src/edu/zeneto/primeiros/projetos/controledefluxo/ExemploBreakContinue.java
@@ -0,0 +1,12 @@
+package edu.zeneto.primeiros.projetos.controledefluxo;
+
+public class ExemploBreakContinue {
+    public static void main(String[] args) {
+       for(int numero = 1; numero <= 5; numero++) {
+           if (numero == 3)
+               //break;
+               continue;
+           System.out.println(numero);
+       }
+    }
+}
---
+++ src/edu/zeneto/primeiros/projetos/controledefluxo/ExemploForArrays.java
@@ -0,0 +1,14 @@
+package edu.zeneto.primeiros.projetos.controledefluxo;
+
+public class ExemploForArrays {
+    public static void main(String[] args) {
+        String alunos [] = {"Felipe", "Jonas", "Julia", "Marcos"};
+       /* for (int x = 0; x < alunos.length; x ++) {
+            System.out.println("O aluno no indice x = " + x + " é " + alunos[x]);} */
+
+        for (String aluno : alunos){
+            System.out.println("Nome  do aluno é " + aluno);
+
+        }
+    }
+}
